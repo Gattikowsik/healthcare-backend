97502ce29d215104d95d89e6ce48b144b82d519f
Gattikowsik <kowsikgatti11@gmail.com>
2025-09-06 01:20:22 +0530

    Updated app.js file

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const authRoutes = require('./routes/authRoutes');
-const patientRoutes = require('./routes/patientRoutes');
-const doctorRoutes = require('./routes/doctorRoutes'); 
-const { authenticate } = require('./middlewares/authMiddleware');
+const express = require("express");
+const dotenv = require("dotenv");
 
+const authRoutes = require("./routes/authRoutes");
+const patientRoutes = require("./routes/patientRoutes");
+const doctorRoutes = require("./routes/doctorRoutes");
+const mappingRoutes = require("./routes/mappingRoutes");
+
+dotenv.config();
 const app = express();
+
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/patients', authenticate, patientRoutes);
-app.use('/api/doctors', authenticate, doctorRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/patients", patientRoutes);
+app.use("/api/doctors", doctorRoutes);
+app.use("/api/mappings", mappingRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Healthcare Backend is running");
+});
 
 module.exports = app;
